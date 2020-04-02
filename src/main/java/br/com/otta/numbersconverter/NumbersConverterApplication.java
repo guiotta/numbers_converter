f10af275dc9013327b4168a60994b1e7e16db80b
Guilherme <guilhermeotta@gmail.com>
2020-04-02 20:37:06 -0300

    Adiciona classes iniciais do projeto.
        - Altera aplicação para rodar no modo console.

---
+++ src/main/java/br/com/otta/numbersconverter/NumbersConverterApplication.java
@@ -0,0 +1,13 @@
+package br.com.otta.numbersconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbersConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbersConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/otta/numbersconverter/NumbersConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.otta.numbersconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbersConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
