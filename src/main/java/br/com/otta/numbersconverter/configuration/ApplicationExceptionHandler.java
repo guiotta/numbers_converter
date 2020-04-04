afc4ab4e09e2655e8175f36a163d2ada04380b59
Guilherme <guilhermeotta@gmail.com>
2020-04-04 11:01:13 -0300

    Adiciona tratamento de erros ao projeto.

---
+++ src/main/java/br/com/otta/numbersconverter/configuration/ApplicationExceptionHandler.java
@@ -0,0 +1,19 @@
+package br.com.otta.numbersconverter.configuration;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
+    protected ResponseEntity<Object> handleBadRequest(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ex.getMessage();
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+}
